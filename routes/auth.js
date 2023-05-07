14cbc611364dc8e834969b4aef3c00bffd51ca69
Chris Schreiner <chrisjschreiner@gmail.com>
2023-05-07 10:43:12 -0700

    "initial commit"

---
+++ routes/auth.js
@@ -0,0 +1,85 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
+
+//REGISTER
+// router.post("/register", async (req, res) => {
+//   const newUser = new User({
+//     username: req.body.username,
+//     email: req.body.email,
+//     password: CryptoJS.AES.encrypt(
+//       req.body.password,
+//       process.env.PASS_SEC
+//     ).toString(),
+//   });
+//   try {
+//     const savedUser = await newUser.save();
+//     res.status(201).json(savedUser);
+//   } catch (err) {
+//     // If want more specific errors, can do with if statements and give specific error code and error .json("You didn't enter an email")
+//     res.status(500).json(err);
+//   }
+// });
+
+//REGISTER
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+  });
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    // If want more specific errors, can do with if statements and give specific error code and error .json("You didn't enter an email")
+    res.status(500).json(err);
+  }
+});
+
+//LOGIN
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    console.log(user);
+    if (!user) {
+      res.status(401).json("Wrong credentials!");
+      return;
+    }
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    if (OriginalPassword !== req.body.password) {
+      res.status(401).json("Wrong credentials!");
+      return;
+    }
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+    //Don't want to show password in database, so destructure user, and pass in all info except for password
+    const { password, ...others } = user._doc;
+
+    res.status(200).json({ ...others, accessToken });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
